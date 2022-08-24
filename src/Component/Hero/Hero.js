aa8c50066fcd6751f51b21421a87d1d21e6b8bc0
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-24 22:09:25 +0500

    Figures Completed

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -19,13 +19,32 @@ const Hero = () => {
           {/*Heading*/}
            <div className="hero-text">
               <div>
-                <span>Shape </span>
+                <span className="stroke-text">Shape </span>
                 <span>Your</span>
               </div>
               <div>
-                <span>Ideal </span>
-                <span>Body</span>
+                <span>Ideal Body</span>
               </div>
+              
+              <div>
+                <span>In here we will help you to shape
+                 and build your ideal body and live up your life to fullest
+                </span>
+              </div>
+           </div>
+           <div className="figures">
+            <div>
+               <span>+ 140</span>
+               <span>expert coaches</span>
+            </div>
+            <div>
+               <span>+ 978</span>
+               <span>members joined</span>
+            </div>
+            <div>
+               <span>+ 50</span>
+               <span> fitness programs</span>
+            </div>
            </div>
         </div>
         <div className="right-h">Right side</div>
