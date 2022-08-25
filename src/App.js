ef0556ec1c4e05accde66d4f1dd49bf55dae62e1
Sameer Shahid <goku.hulk234@gmail.com>
2022-08-25 20:49:57 +0500

    Completed Program Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from './Component/Hero/Hero'
+import Programs from './Component/Programs/Programs'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       <Hero/>
+      <Programs/>
     </div>
   )
 }
---
+++ src/Component/Programs/Programs.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Programs.css'
+
+/*Images*/
+import RightArrow from '../../assets/rightArrow.png'
+
+/*Importing the data from the ProgramData file*/
+import {programsData} from '../../data/programsData'
+
+const Programs = () => {
+  return (
+    <div className="programs" id="programs">
+        {/*Header*/}
+        <div className="programs-header">
+           <span className='stroke-text'>explore our</span>
+           <span>programs</span>
+           <span className='stroke-text'>to shape you</span>
+        </div>
+
+         {/*Programs*/}
+         <div className="program-categories">
+             {programsData.map((data)=>(
+                <div className="category">
+                   {data.image}
+                   <span>{data.heading}</span>
+                   <span>{data.details}</span>
+                   <div className="join-now">
+                    <span>Join Now</span>
+                    <img src={RightArrow} alt="Right Arrow" />
+                   </div>
+                </div>
+             ))}
+         </div>
+    </div>
+  )
+}
+
+export default Programs
